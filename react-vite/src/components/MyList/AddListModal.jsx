c4a92089082371395239283a6265ea75afc05e06
Tyler_Han <tylerhan1226@gmail.com>
2024-09-03 17:15:43 -0700

    start working on add to list

--- react-vite/src/components/MyList/AddListModal.jsx
+++ react-vite/src/components/MyList/AddListModal.jsx
@@ -1,41 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useModal } from "../../context/Modal";
-import { FaSearch } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaHandPointLeft, FaSearch } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 import "./Modal.css"
+import { getAllMyListsThunk } from "../../redux/list";
 
 export default function AddListModal() {
-    const [searchInput, setSearchInput] = useState("")
-    const nav = useNavigate()
     const { closeModal } = useModal()
+    const dispatch = useDispatch()
+    const nav = useNavigate()
+    const user = useSelector(state => state.session.user)
+    const listItems = useSelector(state => state.lists?.My_Lists)
+    const stock = useSelector(state => state.stocks)
+    const stockSymbol = stock?.ticker
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        if (searchInput.length == 0) {
-            nav(`/`)
-        } else {
-            nav(`/search/${searchInput}`)
-        }
+    const listNames = new Set()
+    listItems?.forEach(ele => {
+        listNames.add(ele.list_name)
+    })
+
+    const handle = (e) => {
+        console.log("HANDLE!!!")
         closeModal()
     };
 
+    console.log("listItems ==>", listItems)
+    console.log("listNames ==>", listNames)
+    const test = Array.from(listNames).map((listName) => (
+        listItems.filter(ele => ele.list_name == listName).map(ele => ele.stock_symbol)
+    ))
+    console.log("test ==>", test)
+
+    useEffect(() => {
+        dispatch(getAllMyListsThunk())
+    }, [dispatch])
+
+    if (!user) {
+        return nav('/')
+    }
+
     return (
-        <section id="search-modal-container">
-            <form className="search-form" onSubmit={handleSearch}>
-                <div className="search-form-fields">
-                    <input
-                        className="search-form-field-input"
-                        type="text"
-                        placeholder="Find Your Stock by Symbol"
-                        value={searchInput}
-                        onChange={(e) => setSearchInput(e.target.value)}
-                    />
-                    <button className="search-modal-btn" type="submit">
-                        <p className="search-modal-btn-text"><FaSearch /></p>
-                    </button>
-                </div>
-            </form>
+        <section className="search-modal-container">
+            {Array.from(listNames).map((listName) => (
+                <button
+                className={`add-to-list-btn
+                ${listItems.filter(ele => ele.list_name == listName).map(ele => ele.stock_symbol).includes(stockSymbol)
+                    ? "is-red" : "false"}
+                `}
+                onClick={() => handle()}
+                key={listName}>
+                    {listName}
+                </button>
+            ))}
         </section>
     );
 }
