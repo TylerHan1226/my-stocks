c3cac76fb251116ce41018d322e11093115c374f
Tyler_Han <tylerhan1226@gmail.com>
2024-09-05 11:05:08 -0700

    new list validation

--- react-vite/src/components/MyList/CreateListModal.jsx
+++ react-vite/src/components/MyList/CreateListModal.jsx
@@ -13,52 +13,70 @@ export default function CreateListModal({ stockSymbol }) {
     const listItems = useSelector(state => state.lists?.My_Lists)
 
     const [newListName, setNewListName] = useState('')
-    const [submitted, setSubmitted] = useState(false)
+    const [validations, setValidations] = useState({})
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
     const listNames = new Set()
     listItems?.forEach(ele => {
         listNames.add(ele.list_name)
     })
 
-    console.log("listNames ==>", listNames)
-    console.log("stockSymbol ==>", stockSymbol)
+    const validateForm = () => {
+        const validationErrors = {}
+        if (!newListName || newListName.length > 20) {
+            validationErrors.newListName = 'Please enter a list name between 1 and 20 characters'
+        } else if (listNames.has(newListName)) {
+            validationErrors.newListName = 'A list with this name already exists'
+        }
+        return validationErrors
+    }
+
+    useEffect(() => {
+        if (!user) {
+            return nav('/')
+        }
+        if (isSubmitted) {
+            setValidations(validateForm(newListName, listNames))
+        }
+    }, [newListName, user, isSubmitted])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        setSubmitted(true)
-        // if (Object.keys(validations).length > 0) {
-        //     return
-        // }
-        const newListData = { "list_name": newListName, "stock_symbol": stockSymbol }
-        const newList = await dispatch(addListThunk(newListData))
-        nav(`/my_lists`)
-        closeModal()
+        setIsSubmitted(true)
+        const currentValidations = validateForm()
+        setValidations(currentValidations)
+        
+        if (Object.keys(currentValidations).length > 0) {
+            console.log(currentValidations)
+            return
+        } else {
+            const newListData = { "list_name": newListName, "stock_symbol": stockSymbol }
+            dispatch(addListThunk(newListData))
+            nav(`/my_lists`)
+            closeModal()
+        }
     }
 
     useEffect(() => {
         dispatch(getAllMyListsThunk())
     }, [dispatch])
 
-    if (!user) {
-        return nav('/')
-    }
-
     return (
         <section className="list-modal-container">
             <h2 className="add-list-modal-title">Create a new list for {stockSymbol}!</h2>
-            <form className="add-list-form"
-                onSubmit={handleSubmit}
-            >
+            <form className="add-list-form" onSubmit={handleSubmit}>
                 <label className="add-list-label">
                     <input
                         className="add-list-input-field"
                         type="text"
                         name="newListName"
                         value={newListName}
-                        placeholder="My New List Name"
+                        placeholder="New List Name"
                         onChange={e => setNewListName(e.target.value)}
-                    ></input>
+                    />
                 </label>
+                {isSubmitted && validations.newListName && 
+                <p className="validation-error-text">* {validations.newListName}</p>}
                 <button className="add-to-list-btn not-added">
                     Create My List
                 </button>
