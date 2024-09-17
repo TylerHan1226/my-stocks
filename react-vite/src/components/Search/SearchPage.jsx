d9e070d7a08a10bcadfdac5673eb85968fbb4759
Tyler_Han <tylerhan1226@gmail.com>
2024-09-16 17:49:51 -0700

    chaneg chartPeriod to historical_data_1d

--- react-vite/src/components/Search/SearchPage.jsx
+++ react-vite/src/components/Search/SearchPage.jsx
@@ -12,53 +12,79 @@ Chart.register(annotationPlugin);
 import { makeChart } from "../Helper/Helper";
 
 export default function SearchPage() {
-    const nav = useNavigate();
-    const dispatch = useDispatch();
-    const { searchInput } = useParams();
-    const user = useSelector(state => state.session.user);
-    const stock = useSelector(state => state.stocks);
-    const [isLoading, setIsLoading] = useState(true);
-    const chartRef = useRef(null);
-    const chartInstance = useRef(null);
+    const nav = useNavigate()
+    const dispatch = useDispatch()
+    const { searchInput } = useParams()
+    const user = useSelector(state => state.session.user)
+    const stock = useSelector(state => state.stocks)
+    const [isLoading, setIsLoading] = useState(true)
+    const chartRef = useRef([])
+    const chartInstance = useRef([])
 
-    const stockName = stock?.name;
-    const stockSymbol = stock?.ticker;
-    const stockCurrentPrice = stock?.currentPrice?.toFixed(2);
-    const stockOpenPrice = stock?.info?.previousClose?.toFixed(2);
-    const [chartPeriod, setChartPeriod] = useState('historical_data_1d');
-    const { setModalContent } = useModal();
+    const stockName = stock?.name
+    const stockSymbol = stock?.ticker
+    const stockBusSum = stock?.info?.longBusinessSummary
+    const stockComOfficers = stock?.info?.companyOfficers
+    const stockEmployeeNum = stock?.info?.fullTimeEmployees
+    const stockHeadquarter = `${stock?.info?.city}, ${stock?.info?.state}`
+    const stockCurrentPrice = stock?.currentPrice?.toFixed(2)
+    const stockOpenPrice = stock?.info?.previousClose?.toFixed(2)
+    const stockAsk = stock?.info?.ask
+    const stockAskSize = stock?.info?.askSize
+    const stockBidSize = stock?.info?.bidSize
+    const stockBid = stock?.info?.bid
+    const stockPreviousClosePrice = stock?.info?.open?.toFixed(2)
+    const stockMarketCap = stock?.info?.marketCap
+    const stockPERatio = stock?.info?.trailingPE?.toFixed(2)
+    const stockFwPERatio = stock?.info?.forwardPE?.toFixed(2)
+    const stockVolume = stock?.info?.volume
+    const stockDividendYield = (stock?.info?.dividendYield * 100)?.toFixed(2)
+    const stockYield = (stock?.info?.yield * 100)?.toFixed(2)
+    const stockYieldDailyReturn = (stock?.info?.ytdReturn * 100)?.toFixed(2)
+    const stockNetAssets = stock?.info?.totalAssets
+    const stock50DAvg = stock?.info?.fiftyDayAverage?.toFixed(2)
+    const stockDayHigh = stock?.info?.dayHigh?.toFixed(2)
+    const stockDayLow = stock?.info?.dayLow?.toFixed(2)
+    const stock52WkLow = stock?.info?.fiftyTwoWeekLow?.toFixed(2)
+    const stock52WkHigh = stock?.info?.fiftyTwoWeekHigh?.toFixed(2)
+
+    const [chartPeriod, setChartPeriod] = useState('historical_data_1d')
     console.log(setChartPeriod)
+    const { setModalContent } = useModal()
     const handleOpenModal = () => {
-        setModalContent(<AddListModal stockSymbol={stockSymbol} />);
-    };
+        setModalContent(<AddListModal stockSymbol={stockSymbol} />)
+    }
 
     const handleChartPeriod = () => {
-        console.log('handleChartPeriod clicked!');
-    };
+        console.log('handleChartPeriod clicked!')
+    }
 
     useEffect(() => {
         if (!user) {
-            return nav('/');
+            return nav('/')
         }
+        // setIsLoading(true)
         dispatch(getOneStockThunk(searchInput))
-            .then(() => setIsLoading(false));
-        window.scrollTo(0, 0);
-    }, [nav, dispatch, searchInput, user]);
+            .then(() => setIsLoading(false))
+        window.scrollTo(0, 0)
+    }, [nav, dispatch, searchInput, user])
 
-    const isGreen = stockCurrentPrice > stockOpenPrice ? true : false;
-
+    // chart
+    const isGreen = stockCurrentPrice > stockOpenPrice ? true : false
     useEffect(() => {
         if (stock?.historical_data_1d && chartRef.current) {
-            console.log('Creating chart with data:', stock.historical_data_1d);
-            makeChart(chartPeriod, stock, chartInstance, chartRef, isGreen, setIsLoading);
+            makeChart('historical_data_1d', stock, chartInstance, chartRef, isGreen)
+            // setIsLoading(false)
         }
-    }, [chartPeriod, stock, chartInstance, chartRef, isGreen]);
+        
+    }, [chartPeriod, stock, chartInstance, chartRef, isGreen])
 
     if (isLoading) {
-        return <Loading />;
+        return <Loading />
     }
 
-    console.log('stock ==>', stock);
+    console.log('stock ==>', stock)
+
 
     return (
         <section className="page-container">
@@ -109,8 +135,127 @@ export default function SearchPage() {
                             </button>
                         </div>
                     </div>
+
+                    <h2>About</h2>
+                    <div className="search-info-boxes">
+                        <p>{stockBusSum}</p>
+                        <div className="search-info-about-container">
+                            {stockComOfficers?.length > 0 &&
+                                <p className="search-info-text">
+                                    CEO: {stockComOfficers?.filter(ele => ele.title.includes("CEO"))[0]?.name}
+                                </p>
+                            }
+                            {stockEmployeeNum?.length > 0 &&
+                                <p className="search-info-text">
+                                    Full-time Employees: {stockEmployeeNum}
+                                </p>
+                            }
+                            {stockHeadquarter?.length > 0 &&
+                                <p className="search-info-text">
+                                    Headquarters: {stockHeadquarter}
+                                </p>
+                            }
+                        </div>
+                    </div>
+                    <h2>Key Statistics</h2>
+                    <div className="search-info-boxes">
+                        <div className="search-info-texts-container">
+
+                            <div className="stock-info-column">
+                                <p className="search-info-text">
+                                    Current Price: {!isNaN(stockCurrentPrice) ? `${stockCurrentPrice}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Previous close price: {!isNaN(stockPreviousClosePrice) ? `${stockPreviousClosePrice}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Open price: {!isNaN(stockOpenPrice) ? `${stockOpenPrice}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Bid: {!isNaN(stockBid)
+                                        ? (!isNaN(stockBidSize)
+                                            ? `${stockBid} x ${stockBidSize}`
+                                            : `${stockBid}`)
+                                        : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Ask: {!isNaN(stockAsk)
+                                        ? (!isNaN(stockAskSize)
+                                            ? `${stockAsk} x ${stockAskSize}`
+                                            : `${stockAsk}`)
+                                        : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    50-day average: {!isNaN(stock50DAvg) ? `${stock50DAvg}` : 'N/A'}
+                                </p>
+
+                            </div>
+
+                            <div className="stock-info-column">
+
+                                <p className="search-info-text">
+                                    Volume: {!isNaN(stockVolume)
+                                        ? (stockVolume > 1000000000000
+                                            ? `${(stockVolume / 1000000000000).toFixed(2)}T`
+                                            : stockVolume > 1000000000
+                                                ? `${(stockVolume / 1000000000).toFixed(2)}B`
+                                                : `${(stockVolume / 1000000).toFixed(2)}M`)
+                                        : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    High today: {!isNaN(stockDayHigh) ? `${stockDayHigh}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Low today: {!isNaN(stockDayLow) ? `${stockDayLow}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    52 Week high: {!isNaN(stock52WkHigh) ? `${stock52WkHigh}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    52 Week low: {!isNaN(stock52WkLow) ? `${stock52WkLow}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Market cap: {!isNaN(stockMarketCap) ? (stockMarketCap > 1000000000000
+                                        ? (stockMarketCap / 1000000000000)?.toFixed(2) + 'T'
+                                        : stockMarketCap > 1000000000
+                                            ? (stockMarketCap / 1000000000)?.toFixed(2) + 'B'
+                                            : (stockMarketCap / 1000000)?.toFixed(2) + 'M') : 'N/A'}
+                                </p>
+
+                            </div>
+
+                            <div className="stock-info-column">
+
+                                <p className="search-info-text">
+                                    Net Assets: {!isNaN(stockNetAssets) ? (stockNetAssets > 1000000000000
+                                        ? (stockNetAssets / 1000000000000)?.toFixed(2) + 'T'
+                                        : stockNetAssets > 1000000000
+                                            ? (stockNetAssets / 1000000000)?.toFixed(2) + 'B'
+                                            : (stockNetAssets / 1000000)?.toFixed(2) + 'M') : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Price-Earning ratio: {!isNaN(stockPERatio) ? `${stockPERatio}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Forward Price-Earning ratio: {!isNaN(stockFwPERatio) ? `${stockFwPERatio}` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Dividend Yield: {!isNaN(stockDividendYield) ? `${stockDividendYield}%` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Yield: {!isNaN(stockYield) ? `${stockYield}%` : 'N/A'}
+                                </p>
+                                <p className="search-info-text">
+                                    Yield Daily Total Return: {!isNaN(stockYieldDailyReturn) ? `${stockYieldDailyReturn}%` : 'N/A'}
+                                </p>
+
+                            </div>
+
+                        </div>
+                    </div>
                 </section>
             </section>
+
         </section>
-    );
+    )
 }
