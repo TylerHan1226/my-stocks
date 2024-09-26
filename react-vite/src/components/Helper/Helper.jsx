04158dd3338e4ab22886296f5011dd9325fbfbb5
Tyler_Han <tylerhan1226@gmail.com>
2024-09-26 16:39:38 -0700

    update annotation line

--- react-vite/src/components/Helper/Helper.jsx
+++ react-vite/src/components/Helper/Helper.jsx
@@ -22,12 +22,11 @@ export const makeChart = (period, stocksData, chartInstance, chartRef, isGreen) 
 
     // Find the maximum length of the data arrays
     const maxLength = Math.max(...Object.keys(stocksData).map(stockKey => stocksData[stockKey][period].length))
-
+    let annotationValue = 0
     const datasets = Object.keys(stocksData).map((stockKey, index) => {
         const stock = stocksData[stockKey]
         const chartColor = index === 0 ? (isGreen ? themeGreen : themeRed) : colors[index - 1]
-        let annotationValue
-
+        // update annotationValue
         if (period === 'historical_data_1d') {
             annotationValue = stock?.info?.previousClose
         } else if (['historical_data_1wk', 'historical_data_1mo', 'historical_data_3mo', 'historical_data_6mo', 'historical_data_1yr', 'historical_data_5yr', 'historical_data_10yr'].includes(period)) {
@@ -35,23 +34,19 @@ export const makeChart = (period, stocksData, chartInstance, chartRef, isGreen) 
                 annotationValue = stock[period][0]
             }
         }
-
         if (annotationValue === undefined || annotationValue === null) {
             console.error('Annotation value is not defined')
             annotationValue = 0
         }
-
         // Add currentPrice to the end of the stock[period] array
         if (stock[period] && stock.currentPrice !== undefined) {
             stock[period].push(stock.currentPrice)
         }
-
         // Normalize the length of the data arrays and fill missing values with the last known value
         const normalizedData = [...stock[period]]
         while (normalizedData.length < maxLength) {
             normalizedData.push(normalizedData[normalizedData.length - 1])
         }
-
         return {
             label: stockKey,
             data: normalizedData,
@@ -84,10 +79,10 @@ export const makeChart = (period, stocksData, chartInstance, chartRef, isGreen) 
                     annotations: {
                         line1: {
                             type: 'line',
-                            yMin:  stocksData[Object.keys(stocksData)[0]].info.previousClose,
-                            yMax: stocksData[Object.keys(stocksData)[0]].info.previousClose,
+                            yMin: annotationValue,
+                            yMax: annotationValue,
                             borderColor: lightGrey,
-                            borderWidth: Object.keys(stocksData).length == 1 && period =='historical_data_1d' ? 2 : 0,
+                            borderWidth: 2,
                             borderDash: [6, 6],
                             label: {
                                 content: 'Annotation Line',
@@ -120,20 +115,6 @@ export const makeChartPercentage = (period, stocksData, chartInstance, chartRef,
     const datasets = Object.keys(stocksData).map((stockKey, index) => {
         const stock = stocksData[stockKey]
         const chartColor = index === 0 ? (isGreen ? themeGreen : themeRed) : colors[index - 1]
-        let annotationValue
-
-        if (period === 'historical_data_1d') {
-            annotationValue = stock?.info?.previousClose
-        } else if (['historical_data_1wk', 'historical_data_1mo', 'historical_data_3mo', 'historical_data_6mo', 'historical_data_1yr', 'historical_data_5yr', 'historical_data_10yr'].includes(period)) {
-            if (stock[period] && stock[period].length > 0) {
-                annotationValue = stock[period][0]
-            }
-        }
-
-        if (annotationValue === undefined || annotationValue === null) {
-            console.error('Annotation value is not defined')
-            annotationValue = 0
-        }
         // Add currentPrice to the end of the stock[period] array
         if (stock[period] && stock.currentPrice !== undefined) {
             stock[period].push(stock.currentPrice)
@@ -170,23 +151,6 @@ export const makeChartPercentage = (period, stocksData, chartInstance, chartRef,
             plugins: {
                 legend: { display: Object.keys(stocksData).length == 1 ? false : true },
                 tooltip: { enabled: Object.keys(stocksData).length == 1 ? false : true },
-                annotation: {
-                    annotations: {
-                        line1: {
-                            type: 'line',
-                            yMin: Object.keys(stocksData).length == 1 ? stocksData[Object.keys(stocksData)[0]].info.previousClose : 0,
-                            yMax: Object.keys(stocksData).length == 1 ? stocksData[Object.keys(stocksData)[0]].info.previousClose : 0,
-                            borderColor: lightGrey,
-                            borderWidth: Object.keys(stocksData).length == 1 && period =='historical_data_1d' ? 2 : 0,
-                            borderDash: [6, 6],
-                            label: {
-                                content: 'Annotation Line',
-                                enabled: true,
-                                position: 'end',
-                            },
-                        },
-                    },
-                },
             },
         },
     })
@@ -196,70 +160,69 @@ export const makeChartPercentage = (period, stocksData, chartInstance, chartRef,
 export const makeChartSmall = (stock, chartInstance, chartRef, isGreen) => {
     const period = 'historical_data_1d'
     if (chartInstance && chartInstance.current) {
-      chartInstance.current.destroy()
+        chartInstance.current.destroy()
     }
-    if (!stock) return 
+    if (!stock) return
     // Set the canvas size to 50% of its original size
     chartRef.width = chartRef.width / 2
     chartRef.height = chartRef.height / 2
-  
+
     const ctx = chartRef.getContext('2d')
     const themeGreen = getComputedStyle(document.documentElement).getPropertyValue('--theme-green').trim()
     const themeRed = getComputedStyle(document.documentElement).getPropertyValue('--theme-red').trim()
     const lightGrey = getComputedStyle(document.documentElement).getPropertyValue('--text-field-grey').trim()
     const chartColor = isGreen ? themeGreen : themeRed
     let annotationValue
-  
+
     annotationValue = stock?.info?.previousClose || 0
-    console.log('stock in small chart ==>', [stock?.ticker, stock?.info?.previousClose])
     // Add currentPrice to the end of the stock[period] array
     if (stock[period] && stock.currentPrice !== undefined) {
-      stock[period].push(stock.currentPrice)
+        stock[period].push(stock.currentPrice)
     }
-  
+
     chartInstance.current = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Array(stock[period]?.length).fill(''),
-        datasets: [{
-          data: stock[period],
-          borderColor: chartColor,
-          fill: false,
-          borderWidth: 2,
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: { display: false },
-          y: { display: false }
+        type: 'line',
+        data: {
+            labels: Array(stock[period]?.length).fill(''),
+            datasets: [{
+                data: stock[period],
+                borderColor: chartColor,
+                fill: false,
+                borderWidth: 2,
+            }]
         },
-        elements: {
-          point: { radius: 0 }
-        },
-        plugins: {
-          legend: { display: false },
-          tooltip: { enabled: true },
-          annotation: {
-            annotations: {
-              line1: {
-                type: 'line',
-                yMin: annotationValue,
-                yMax: annotationValue,
-                borderColor: lightGrey,
-                borderWidth: 1,
-                borderDash: [6, 6],
-                label: {
-                  content: 'Annotation Line',
-                  enabled: true,
-                  position: 'end'
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: { display: false },
+                y: { display: false }
+            },
+            elements: {
+                point: { radius: 0 }
+            },
+            plugins: {
+                legend: { display: false },
+                tooltip: { enabled: true },
+                annotation: {
+                    annotations: {
+                        line1: {
+                            type: 'line',
+                            yMin: annotationValue,
+                            yMax: annotationValue,
+                            borderColor: lightGrey,
+                            borderWidth: 1,
+                            borderDash: [6, 6],
+                            label: {
+                                content: 'Annotation Line',
+                                enabled: true,
+                                position: 'end'
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
     })
-  }
+}
 
